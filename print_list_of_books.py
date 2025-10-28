157d2eb741e2a4c379c6155abb3757bde8369689
lavander8 <lavander8@rambler.su>
2025-10-28 14:45:26 +0300

    Добавлена функция add_book

--- print_list_of_books.py
+++ print_list_of_books.py
@@ -6,6 +6,20 @@ def book_list_view(library):
     print("Список книг в библиотеке:\n" + "\n".join(library))
 
 
+def add_book(library, title, author, year):
+    if title in library:
+        print(f"Книга '{title}' уже существует в библиотеке.")
+        choice = input("Хотите обновить информацию о книге? (да/нет): ").strip().lower()
+        if choice == "да":
+            library[title] = {"author": author, "year": year, "availability": None}
+            print(f"Информация о книге '{title}' успешно обновлена.\n")
+        else:
+            print("Информация о книге не изменена.\n")
+    else:
+        library[title] = {"author": author, "year": year, "availability": None}
+        print(f"Книга '{title}' успешно добавлена в библиотеку.\n")
+
+
 library = {
     "Wuthering Heights": {"author": "Emily Brontë",
                           "year": 1847,
@@ -28,6 +42,9 @@ library = {
 }
 
 empty_library = {}
+
+add_book(library, "Jane Eyre", "Charlotte Brontë", 1847)
+add_book(library, "The Divine Comedy", "Dante Alighieri", 1472)
 
 book_list_view(library)
 book_list_view(empty_library)
