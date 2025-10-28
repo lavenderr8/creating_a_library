e0298e90028e1d0981241006d787b972b75925f9
lavander8 <lavander8@rambler.su>
2025-10-28 16:24:35 +0300

    Отредактирована запись в словаре

--- print_list_of_books.py
+++ print_list_of_books.py
@@ -1,6 +1,6 @@
 def book_list_view(library):
     if not library:
-        print("В библиотеке нет книг.")
+        print(f"В библиотеке нет книг.")
         return
 
     print("Список книг в библиотеке:\n" + "\n".join(library))
@@ -11,40 +11,52 @@ def add_book(library, title, author, year):
         print(f"Книга '{title}' уже существует в библиотеке.")
         choice = input("Хотите обновить информацию о книге? (да/нет): ").strip().lower()
         if choice == "да":
-            library[title] = {"author": author, "year": year, "availability": None}
+            library[title] = {"Автор": author, "Год издания": year, "Наличие": None}
             print(f"Информация о книге '{title}' успешно обновлена.\n")
         else:
             print("Информация о книге не изменена.\n")
     else:
-        library[title] = {"author": author, "year": year, "availability": None}
+        library[title] = {"Автор": author, "Год издания": year, "Наличие": None}
         print(f"Книга '{title}' успешно добавлена в библиотеку.\n")
 
 
 library = {
-    "Wuthering Heights": {"author": "Emily Brontë",
-                          "year": 1847,
-                          "availability": "available"},
-    "The Sorrows of Satan": {"author": "Marie Corelli",
-                             "year": 1895,
-                             "availability": "available"},
-    "The Screwtape Letters": {"author": "Clive Staples Lewis",
-                              "year": 1942,
-                              "availability": "not available"},
-    "And Quiet Flows the Don": {"author": "Mikhail Sholokhov",
-                                "year": 1928,
-                                "availability": "not available"},
-    "Notre-Dame de Paris": {"author": "Victor Hugo",
-                            "year": 1831,
-                            "availability": "available"},
-    "Jane Eyre": {"author": "Charlotte Brontë",
-                  "year": 1847,
-                  "availability": "available"}
+    "Грозовой перевал": {
+        "Автор": "Эмили Бронте",
+        "Год издания": 1847,
+        "Наличие": "В наличии"
+    },
+    "Скорбь Сатаны": {
+        "Автор": "Мария Корелли",
+        "Год издания": 1895,
+        "Наличие": "В наличии"
+    },
+    "Письма Баламута": {
+        "Автор": "Клайв Стейплз Льюис",
+        "Год издания": 1942,
+        "Наличие": "Нет в наличии"
+    },
+    "Тихий Дон": {
+        "Автор": "Михаил Шолохов",
+        "Год издания": 1928,
+        "Наличие": "Нет в ниличии"
+    },
+    "Собор Парижской Богоматери": {
+        "Автор": "Викор Гюго",
+        "Год издания": 1831,
+        "Наличие": "В наличии"
+    },
+    "Джейн Эйр": {
+        "Автор": "Шарлотта Бронте",
+        "Год издания": 1847,
+        "Наличие": "В наличии"
+    },
 }
 
 empty_library = {}
 
-add_book(library, "Jane Eyre", "Charlotte Brontë", 1847)
-add_book(library, "The Divine Comedy", "Dante Alighieri", 1472)
+add_book(library, "Джейн Эйр", "Шарлотта Бронте", 1847)
+add_book(library, "Божественная комедия", "Данте Алигьери", 1472)
 
 book_list_view(library)
 book_list_view(empty_library)
